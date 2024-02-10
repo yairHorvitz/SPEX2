1714af93f4e437ccb13f1d65995f51d736f00c71
Yair Horvitz <yairovitz@gmail.com>
2024-02-10 23:03:40 +0200

    update

--- my_mat.c
+++ my_mat.c
@@ -7,41 +7,58 @@
 void matrixVarible(int matrix[ROWS][COLS])
 {
     int x;
-    for (size_t i = 0; i < 10; i++)
+    for (size_t i = 0; i < COLS; i++)
     {
-        for (size_t j = 0; j < 10; j++)
+        for (size_t j = 0; j < ROWS; j++)
         {
           scanf("%d",&x);  /* code */
           matrix[i][j]=x;
         }
         
     }
-    
+        
 }
 
 void pathExist(int matrix[ROWS][COLS]) 
 {
-  for(int i=0 ;i<COLS;i++)
+for(int i=0 ;i<COLS;i++)
 {
-for(int j=0;j<ROWS;j++){
-  if(matrix[i][j]==0){matrix[i][j]=__INT_MAX__;}
-}
+  for(int j=0;j<ROWS;j++){
+    if(i == j || matrix[i][j] == 0)
+    {matrix[i][j]=__INT_MAX__;}
+   // printf("%d ",matrix[i][j]);
+    }
 }
 int c=0;
 int r=0;
 scanf("%d",&c);
 scanf("%d",&r);
+
 for (int k = 0; k < COLS; k++)     //number of edges
 {
   for (int i = 0; i < COLS; i++)   
   {
     for (int j = 0; j < COLS; j++)
     {
-      if(matrix[i][j]>matrix[i][k]+matrix[k][j])
-      {matrix[i][j]=matrix[i][k]+matrix[k][j];}
+       if(i == j || matrix[i][j] == 0){matrix[i][j] = __INT_MAX__;}
+       else if(matrix[i][k]==__INT_MAX__||matrix[k][j]==__INT_MAX__){continue;}
+    else  if(matrix[i][j]> matrix[i][k]+matrix[k][j])
+    { // printf("befor ij:  %d\n", matrix[i][j]);
+    //  printf("befor ik:  %d\n", matrix[i][k]);
+     // printf("befor jk:  %d\n", matrix[k][j]);
+    
+      {matrix[i][j] =matrix[i][k]+matrix[k][j];}
+      // printf("ij:  %d\n", matrix[i][j]);
+   //   printf("ik:  %d\n", matrix[i][k]);
+    //  printf("jk:  %d\n", matrix[k][j]);
+      }
     }
   }
 }
+ // for (size_t i = 0; i < 10; i++)
+   //     for (size_t j = 0; j < 10; j++)
+       // printf("after the shortest %d\n", matrix[i][j]);
+
 if(matrix[c][r]!=__INT_MAX__){printf("TRUE\n");}
 else{printf("FALSE\n");}
 
@@ -55,7 +72,7 @@ int shortestPath (int matrix[ROWS][COLS])
 for(int i=0 ;i<COLS;i++)
 {
 for(int j=0;j<ROWS;j++){
-  if((matrix[i][j]==0)||(i=j)){matrix[i][j]=__INT_MAX__;}
+  if(matrix[i][j]==0||i==j){matrix[i][j]=__INT_MAX__;}
 }
 }
 int c=0;
@@ -68,8 +85,10 @@ for (int k = 0; k < COLS; k++)     //number of edges
   {
     for (int j = 0; j < COLS; j++)
     {
-      if(matrix[c][r]>matrix[c][k]+matrix[k][r])
-      {matrix[c][r]=matrix[c][k]+matrix[k][r];}
+       if(matrix[i][j]==0||i==j){matrix[i][j]=__INT_MAX__;}
+       else if(matrix[i][k]==__INT_MAX__||matrix[k][j]==__INT_MAX__){continue;}
+      else if(matrix[i][j]>matrix[i][k]+matrix[k][j])
+      {matrix[i][j]=matrix[i][k]+matrix[k][j];}
     }
   }
 }
